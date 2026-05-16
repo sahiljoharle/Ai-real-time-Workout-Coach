14edf18f8e722ca40e978e7f313d64ae63d859ee
Sahil Joharle <sahil@naveraventures.com>
2026-05-17 00:02:19 +0530

    Side-Bar implemented

--- services/auth/login_wall.py
+++ services/auth/login_wall.py
@@ -1,20 +1,26 @@
-import streamlit as st 
+import streamlit as st
+
 
 def render_login_wall():
     if st.session_state.get("user_id") is not None:
         return True
     
-    st.title("Ai real-time Workout Coach")
-    st.markdown("### Welcome! Please Enter Your Usename ")
+    st.title("🏋️‍♂️ AI Real-time GYM Trainer")
+    st.markdown("### Welcome! Please enter a username to start.")
 
     with st.form("login_form", clear_on_submit=False):
-        username - st.text_input("Username", placeholder="Unique name to identify you")
-        submit_button = st.form_submit_button("Submit", width="stretch")
-    
+        username = st.text_input("Name (unique)", placeholder="unique name e.g. princekhunt")
+        submit_button = st.form_submit_button("Start Session", width="stretch")
+
     if submit_button:
         if not username:
-            st.error("Username cannot be empty.")
+            st.error("Name cannot be empty.")
             return False
-        st.session_state["user_id"] = username
+    
+        st.session_state["username"] = username
+        st.session_state["user_id"] = "1"
+
+        st.rerun()
 
     return False
+
